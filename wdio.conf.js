b661e1f0e60590036c54b3199bb5f8c2a92cb64e
Utkarsh-Honparkhe <utkarsh.honparkhe@ibm.com>
2021-08-05 23:48:21 +0530

    c11

--- wdio.conf.js
+++ wdio.conf.js
@@ -32,6 +32,8 @@ if (postSlackWebhookURL == null)
 postSlackWebhookURL = "https://hooks.slack.com/services/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"
 
 exports.config = {
+    user: 'utkarshhonparkhe_g1O38H',
+    key: 'G5Kempg2f1z78yzpvXQs',
 
     //=====================
     // parameters
@@ -127,6 +129,10 @@ exports.config = {
     // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'info',
     outputDir: 'wdio-logs',
+    waitforTimeout: 30000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    host: 'hub.browserstack.com',
     //
     // Set specific log levels per logger
     // loggers:
