9636e21160a83523892dddc295ba273f599c0ecd
Utkarsh-Honparkhe <utkarsh.honparkhe@ibm.com>
2021-08-06 11:26:42 +0530

    c15

--- e2e/specs/login.spec.js
+++ e2e/specs/login.spec.js
@@ -1,17 +1,15 @@
-var loginPage = require("../pageobjects/login.pageobject.js");
 var launchpadPage = require("../pageobjects/launchpad.pageobject.js");
 var catalogPage = require("../pageobjects/catalog.pageobject.js");
 var ordersPage =  require("../pageobjects/orders.pageobject.js");
 var launchpadPageJson = require("../../testData/launchpadPage.json");
 var catalogPageJson =  require("../../testData/catalogPage.json");
 var ordersPageJson =  require("../../testData/ordersPage.json");
-var appUtils = require("../../common-utils/appUtils.js");
 
 describe('Sanity Tests for MCMP', function(){
     
-    // beforeAll(async function(){
-    //     expect(await launchpadPage.getMCMPHeaderTitle()).toBe(launchpadPageJson.mcmpHeaderText);
-    // });
+    beforeAll(async function(){
+        expect(await launchpadPage.getMCMPHeaderTitle()).toBe(launchpadPageJson.mcmpHeaderText);
+    });
 
     it('Go to Catalog page', async function(){
         await catalogPage.open();
